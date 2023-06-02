29a9d0c704313c32d9bffb2f94f108828183005b
rubel-sh <rubel.hossain0619@gmail.com>
2023-06-03 02:17:51 +0600

    [done] cors, parser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Application, Request, Response } from "express";
-
+import cors from "cors";
 const app: Application = express();
+
+app.use(cors());
+// Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
