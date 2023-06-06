c3585d2567c9d12c745d1ec00414737dcb3a07c9
rubel-sh <rubel.hossain0619@gmail.com>
2023-06-07 02:30:20 +0600

    setup eslint, prettier, husky, lint-staged

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,10 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv'
+import path from 'path'
 
 // Set .env path as we are inside src folder
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-};
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+}
