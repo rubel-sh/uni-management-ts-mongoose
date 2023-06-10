54161feef13429fa3d27981943ef6a14b9bf82b0
rubel-sh <rubel.hossain0619@gmail.com>
2023-06-10 17:25:40 +0600

    fix eslint errors

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import logger from './shared/logger'
 
 async function dbconnect() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`Database connected`)
+    logger.info('Database connected')
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      logger.info(`Example app listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('failed to connect', err)
+    logger.error('failed to connect', err)
   }
 }
 
