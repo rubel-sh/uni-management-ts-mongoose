abb462298cf12fafecf340dc6b08e3c7c51566de
rubel-sh <rubel.hossain0619@gmail.com>
2023-06-25 17:43:45 +0600

    Handling same year and same semester issue

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,4 +1,6 @@
+import status from 'http-status';
 import { Schema, model } from 'mongoose';
+import ApiError from '../../../errors/ApiError';
 import {
   academicSemesterCodes,
   academicSemesterMonths,
@@ -41,7 +43,29 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
   }
 );
 
+// Mongoose prehook
+academicSemesterSchema.pre('save', async function (next) {
+  // 👈 this is a mongoose prehook and next aswell
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(status.CONFLICT, 'Semester already exists');
+  }
+  next();
+});
+
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
   academicSemesterSchema
 );
+
+// Handling same year and same semester issue
+
+// Mongoose : prehook & posthook
+// Prehook : before saving the data
+// Posthook : after saving the data
+
+// Prehook check: Data -> check ? same year and same semester -> throw error
+//
