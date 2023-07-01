66fa3b6dca0e3b962672a33abc2b26f9c5a45852
rubel-sh <rubel.hossain0619@gmail.com>
2023-07-01 09:53:39 +0600

    academic faculty created

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 import { UserRoutes } from '../modules/user/user.route';
 
@@ -8,6 +9,7 @@ const router = express.Router();
 const moduleRoutes: { path: string; route: Router }[] = [
   { path: '/users', route: UserRoutes },
   { path: '/academic-semesters', route: AcademicSemesterRoutes },
+  { path: '/academic-faculties', route: AcademicFacultyRoutes },
 ];
 
 moduleRoutes.forEach(route => {
