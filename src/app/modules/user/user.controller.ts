3e07f1554123fa8aec573ce628a7360825825ada
rubel-sh <rubel.hossain0619@gmail.com>
2023-07-02 11:44:41 +0600

    create-student and populate referance fields

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -5,10 +5,11 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { UserService } from './user.service';
 
-const createUser = catchAsync(async (req: Request, res: Response) => {
+const createStudent = catchAsync(async (req: Request, res: Response) => {
   // Mongoose stuff
-  const { user } = req.body; // user.role
-  const result = await UserService.createUser(user);
+
+  const { student, ...userData } = req.body; // user.role
+  const result = await UserService.createStudent(student, userData);
 
   sendResponse(res, {
     success: true,
@@ -18,4 +19,4 @@ const createUser = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-export const UserController = { createUser };
+export const UserController = { createStudent };
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,18 +1,18 @@
 import { IAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { User } from './user.model';
 
-export const findLastStudentId = async () => {
-  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+export const findLastStudentId = async (): Promise<string | undefined> => {
+  const lastStudent = await User.findOne({ role: 'student' }, { id: 1, _id: 0 })
     .sort({
       createdAt: -1, //descending order
     })
     .lean(); //returns pure js object
 
-  return lastUser?.id;
+  return lastStudent?.id ? lastStudent.id.substring(4) : undefined;
 };
 
 export const generateStudentId = async (
-  academicSemester: IAcademicSemester
+  academicSemester: IAcademicSemester | null
 ): Promise<string> => {
   const currentId =
     (await findLastStudentId()) || (0).toString().padStart(5, '0'); // create 00001 or 00002 if user already exist
@@ -21,9 +21,28 @@ export const generateStudentId = async (
   let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0'); // convert currentId to number then add 1 then convert to string and padStart
 
   // 2025 + 01 + 0001 = 25010001
-  incrementedId = `${academicSemester.year.substring(2)}${
-    academicSemester.code
-  }${incrementedId}`;
+  if (academicSemester !== null) {
+    incrementedId = `${academicSemester.year.substring(2)}${
+      academicSemester.code
+    }${incrementedId}`;
+  }
 
+  return incrementedId;
+};
+
+export const findLastFacultyId = async (): Promise<string | undefined> => {
+  const lastFaculty = await User.findOne({ role: 'faculty' }, { id: 1, _id: 0 })
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+  return lastFaculty?.id ? lastFaculty.id.substring(2) : undefined;
+};
+
+export const generateFacultyId = async (): Promise<string> => {
+  const currentId =
+    (await findLastFacultyId()) || (0).toString().padStart(5, '0');
+  let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+  incrementedId = `F-${incrementedId}`;
   return incrementedId;
 };
